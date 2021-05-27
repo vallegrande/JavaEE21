fbeb5ebef20f1eb908c035f3cff51b89d006d844
jhordycg <jhordycg@pm.me>
2021-05-27 16:31:31 -0500

    refactoring 2

--- buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t pe.edu.vallegrande.taller.javaee21/SonarCloud .
-docker rm -f SonarCloud || true && docker run -d -p 9080:9080 -p 9443:9443 --name SonarCloud pe.edu.vallegrande.taller.javaee21/SonarCloud
